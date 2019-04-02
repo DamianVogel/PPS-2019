d40bfbdcd25ba86b06755d20ed98a9904721d54a
DamianVogel <rodolfovogel@gmail.com>
2019-04-02 20:28:40 -0300

    Se agrega FireBase a consorcio

--- consorcioApp/src/app/app.component.ts
+++ consorcioApp/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase';
+import {FIREBASE_CONFIG} from './enviroment';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -15,6 +18,7 @@ export class AppComponent {
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    firebase.initializeApp(FIREBASE_CONFIG);
   }
 
   initializeApp() {
--- consorcioApp/src/app/home/home.page.ts
+++ consorcioApp/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import {ListaUsuarios} from '../../app/enviroment'; 
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  email: string;
+  clave: string;
+
+  usuarios : any;
+
+  ref = firebase.database().ref('usuarios/');
+
+  constructor(){
+   
+  }
+
+  enviar(){
+    this.ref.on('value', resp => {    
+      this.usuarios = ListaUsuarios(resp);      
+      
+      for(let usuario of this.usuarios){
+        if(usuario.email == this.email && usuario.clave == this.clave){
+          alert("identificado");
+          break;
+        }
+      }      
+      alert("Email o clave incorrecta");   
+    });
+
+  }
 }
