49d1f94c47d1f7e2344d56413c18b047318d7934
DamianVogel <rodolfovogel@gmail.com>
2019-04-10 17:09:58 -0300

    Consorcio App Animado

--- consorcioApp/src/app/app.component.ts
+++ consorcioApp/src/app/app.component.ts
@@ -7,11 +7,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import * as firebase from 'firebase';
 import {FIREBASE_CONFIG} from './enviroment';
 
+import { timer } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  
+  showSplash = true;
+  
+  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -25,6 +31,9 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      timer(3000).subscribe(() => this.showSplash = false)
+
     });
   }
 }
