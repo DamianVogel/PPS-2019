0887141f4910b64c78aab5b08367ca5792b3eca7
DamianVogel <rodolfovogel@gmail.com>
2019-04-16 18:03:53 -0300

    AnotadorMetegol

--- AnotadorMetegol/src/app/app.module.ts
+++ AnotadorMetegol/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,10 +10,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ConexionUsuariosPage } from '../app/conexion-usuarios/conexion-usuarios.page';
+import {ConexionUsuariosPageModule} from '../app/conexion-usuarios/conexion-usuarios.module';
+
+// import { MenuPage} from '../app/menu/menu.page';
+// import { MenuPageModule } from '../app/menu/menu.module';
+
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  entryComponents: [ConexionUsuariosPage],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ConexionUsuariosPageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
