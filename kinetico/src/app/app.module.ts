046745d3729721f0d7292ac1e6a02a22507af5e2
DamianVogel <rodolfovogel@gmail.com>
2019-05-09 21:29:56 -0300

    Page y componentes!

---
+++ kinetico/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { ConexionUsuariosPage } from '../app/conexion-usuarios/conexion-usuarios.page';
+import {ConexionUsuariosPageModule} from '../app/conexion-usuarios/conexion-usuarios.module';
+
+import { GaleriaPageModule } from '../app/galeria/galeria.module';
+import { GaleriaPage } from '../app/galeria/galeria.page';
+
+import { MenuPage} from '../app/menu/menu.page';
+import { MenuPageModule } from '../app/menu/menu.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [ConexionUsuariosPage, GaleriaPage],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    GaleriaPageModule,
+    ConexionUsuariosPageModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ kinetico/src/app/conexion-usuarios/conexion-usuarios.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conexion-usuarios',
+  templateUrl: './conexion-usuarios.page.html',
+  styleUrls: ['./conexion-usuarios.page.scss'],
+})
+export class ConexionUsuariosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
